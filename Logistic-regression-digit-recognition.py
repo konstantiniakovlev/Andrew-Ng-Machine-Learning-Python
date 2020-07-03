b41d5ec3803509653ed178a78b52d6b8a7f56871
konstantiniakovlev <62013320+konstantiniakovlev@users.noreply.github.com>
2020-07-03 17:16:39 +0200

    Update and rename ML_Digits_log_reg.py to Logistic-regression-digit-recognition.py

--- Logistic-regression-digit-recognition.py
+++ Logistic-regression-digit-recognition.py
@@ -7,11 +7,12 @@ from sklearn import metrics
 
 data = loadmat('ML\\ex3data1.mat',)
 
+# data splitting
 X = data['X']
-X = np.insert(X, 0, values = np.ones(X.shape[0]), axis = 1 ) #(5000,401)
-y = data['y'] #(5000,1)
+X = np.insert(X, 0, values = np.ones(X.shape[0]), axis = 1 )  # (5000,401)
+y = data['y']  # (5000,1)
 
-
+# sigmoid function
 def Sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
@@ -19,14 +20,14 @@ lamb = 1
 parameters = X.shape[1]
 theta = np.zeros(parameters)
 
-def LossFunction(theta, X, y, lamb): #scalar
+# loss function
+def LossFunction(theta, X, y, lamb):  # scalar
     m = len(X)
-    theta = np.matrix(theta)#(1,5000)
+    theta = np.matrix(theta)  # (1,5000)
     X = np.matrix(X)
     y = np.matrix(y)
     
-    "loss function"
-    sigmoid = Sigmoid(X * theta.T) #(5000,1)  
+    sigmoid = Sigmoid(X * theta.T)  # (5000,1)  
     loss_term = y.T * np.log(sigmoid) + (1-y).T * np.log(1-sigmoid)
     reg = lamb * (theta * (theta.T))
     loss = - ( loss_term / m ) + (reg / (2*m))
@@ -34,6 +35,7 @@ def LossFunction(theta, X, y, lamb): #scalar
 
 print("Loss before optimization: ", int(LossFunction(theta,X,y,lamb)))
 
+# gradient of the loss function
 def Gradient(theta, X, y, lamb):
     m = len(X)
     theta = np.matrix(theta)
@@ -51,7 +53,8 @@ def Gradient(theta, X, y, lamb):
 number_of_labels = len(np.unique(y)) #10
 parameters = int(X.shape[1])
 all_theta = np.zeros((number_of_labels , parameters))
-    
+
+# one vs all classification
 def oneVSall(X, y, lamb):
     number_of_labels = len(np.unique(y)) #10
     parameters = int(X.shape[1])
@@ -64,24 +67,26 @@ def oneVSall(X, y, lamb):
         print(i)
         theta_i = optimize.minimize(fun = LossFunction, x0 = theta, args = (X, y_i, lamb), method = 'TNC', jac = Gradient)
         for j in range(parameters):
-            all_theta[i-1,j] = theta_i['x'][j] #theta_i['x'] (401,)
+            all_theta[i-1,j] = theta_i['x'][j]  # theta_i['x'] (401,)
             
     return all_theta
     
-full_theta = oneVSall(X, y, lamb) #(10,401)
+full_theta = oneVSall(X, y, lamb)  # (10,401)
 
+# get predictions
 def predictions(X, theta):
     X = np.matrix(X)
     theta = np.matrix(theta)
     z = X * theta.T
-    h = Sigmoid(z) # (5000, 10)
+    h = Sigmoid(z)  # (5000, 10)
     
-    best = np.argmax(h, axis = 1) + 1 #(5000,1)
+    best = np.argmax(h, axis = 1) + 1  # (5000,1)
     
     return best
     
 prediction = predictions(X, full_theta)
 
+# determine accuracy
 def AccuracyScore(y, prediction):
     score_list = []
     for i in range(y.shape[0]):
@@ -94,15 +99,7 @@ def AccuracyScore(y, prediction):
     accuracy_score = np.sum(score_list) / len(score_list)
     return accuracy_score
 
+# determine score
 score = AccuracyScore(y, prediction) * 100
+
 print("Regularization parameter: ", lamb, "\nAccuracy score: ", score,"%\nOptimization model: TNC")
-    
-
-
-    
-    
-    
-    
-    
-
-
